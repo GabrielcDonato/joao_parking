c4e9481fe0667c2b851ed649433b9258622c1f68
Gabriel Donato <gabrielcdonato@gmail.com>
2022-03-10 14:12:04 -0300

    Feat:(APP) app widget separated from main

---
+++ lib/app_widget.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:joao_parking/core/router/router.dart';
+
+import 'core/core.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(builder: (context, constraints) {
+      AppSize().init(constraints);
+
+      return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'João Parking',
+        theme: ThemeData(primarySwatch: Colors.green),
+        initialRoute: '/splash_page',
+        onGenerateRoute: RouteGenerator.generateRoute,
+      );
+    });
+  }
+}
